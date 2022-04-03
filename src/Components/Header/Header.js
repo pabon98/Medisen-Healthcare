79eeb6a3faacf028dc10ce0b1a50420d79a01746
pabon98 <hasinanjumpabon@gmail.com>
2022-04-03 11:53:34 +0600

    updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,16 +17,17 @@ const {user,logOut}=useAuth();
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            data-bs-target="#toogleMobileMenu"
+            aria-controls="toogleMobileMenu"
+            aria-expanded='false'
+            aria-label='Toogle navigation'
+           
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
             className="collapse navbar-collapse"
-            id="navbarSupportedContent Nav_menu"
+            id="toogleMobileMenu"
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
